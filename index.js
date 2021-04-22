40844f7394b5c990b683d5246cab9fdcf923c75d
Mukul Shakya <muklshakya123@gmail.com>
2021-04-22 19:45:21 +0530

    timezone test

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ bot.action(/pricehistory_.*/, async (ctx) => {
     let caption = `*Title: *${subscription.title}\n\n*Current Price: *₹${subscription.currentPrice}\n\n*Url: *${subscription.url}\n\n*Price Histories: *\n\n`;
     subscription.priceHistories.forEach((item) => {
       caption += `₹${item.price} @ ${new Date(item.datetime).toLocaleString({
-        timezone: "Asia/Kolkata",
+        timeZone: "Asia/Kolkata",
       })}\n\n`;
     });
     await ctx.answerCbQuery();
