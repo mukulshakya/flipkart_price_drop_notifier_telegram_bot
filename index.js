9a46c9b853c67a2171f4a564b369be529b5db28a
Mukul Shakya <muklshakya123@gmail.com>
2021-04-22 19:35:56 +0530

    Added price history

--- index.js
+++ index.js
@@ -33,27 +33,35 @@ bot.command("list_alerts", async (ctx) => {
       username: ctx.message.chat.username,
     });
     for (const subscription of subscriptions) {
-      await ctx.telegram.sendPhoto(ctx.message.chat.id, subscription.imageUrl, {
-        parse_mode: "Markdown",
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: "Delete Alert?",
-                callback_data: "delete_alert_" + subscription._id,
-                hide: false,
-              },
+      await ctx.telegram.sendPhoto(
+        ctx.message.chat.id,
+        "https://images-na.ssl-images-amazon.com/images/I/71wkpcIfqdL._SL1500_.jpg",
+        {
+          parse_mode: "Markdown",
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "Delete Alert?",
+                  callback_data: "delete_alert_" + subscription._id,
+                  hide: false,
+                },
+              ],
+              [
+                {
+                  text: "View Price History?",
+                  callback_data: "pricehistory_" + subscription._id,
+                  hide: false,
+                },
+              ],
             ],
-          ],
-        },
-        caption: `*Title: *${subscription.title}\n\n*Current Price: *₹${subscription.currentPrice}\n\n*Url: *${subscription.url}`,
-      });
+          },
+          caption: `*Title: *${subscription.title}\n\n*Current Price: *₹${subscription.currentPrice}\n\n*Url: *${subscription.url}`,
+        }
+      );
     }
     return subscriptions.length
-      ? ctx.reply(
-          "Click *Delete Alert?* button below the listing to stop tracking.",
-          { parse_mode: "Markdown" }
-        )
+      ? null
       : ctx.reply(
           "You don't have any alert set, Paste link to start tracking.",
           { parse_mode: "Markdown" }
@@ -133,9 +141,40 @@ bot.action(/delete_alert_.*/, async (ctx) => {
   try {
     const id = ctx.callbackQuery.data.replace("delete_alert_", "");
     await db.Subscription.findByIdAndDelete(id);
-    await ctx.answerCbQuery();
     await ctx.deleteMessage(ctx.callbackQuery.message.message_id);
-    return await ctx.reply("Alert Deleted!");
+    await ctx.reply("Alert Deleted!");
+    return await ctx.answerCbQuery();
+  } catch (e) {
+    console.log(e);
+    return ctx.reply("Some unexpected error occurred! Please try again.");
+  }
+});
+
+bot.action(/pricehistory_.*/, async (ctx) => {
+  try {
+    const id = ctx.callbackQuery.data.replace("pricehistory_", "");
+    const subscription = await db.Subscription.findById(id);
+    let caption = `*Title: *${subscription.title}\n\n*Current Price: *₹${subscription.currentPrice}\n\n*Url: *${subscription.url}\n\n*Price Histories: *\n\n`;
+    subscription.priceHistories.forEach((item) => {
+      caption += `₹${item.price} @ ${new Date(
+        item.datetime
+      ).toLocaleString()}\n\n`;
+    });
+    await ctx.answerCbQuery();
+    return await ctx.editMessageCaption(caption, {
+      parse_mode: "Markdown",
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Delete Alert?",
+              callback_data: "delete_alert_" + subscription._id,
+              hide: false,
+            },
+          ],
+        ],
+      },
+    });
   } catch (e) {
     console.log(e);
     return ctx.reply("Some unexpected error occurred! Please try again.");
--- notifier.js
+++ notifier.js
@@ -9,43 +9,52 @@ module.exports = (bot, db) => {
       const subscriptions = await db.Subscription.find();
       for (const subscription of subscriptions) {
         const { pricing } = await flipkartScrapper(subscription.url);
-
-        if (
-          Number(pricing) < subscription.currentPrice &&
-          subscription.currentPrice <= subscription.initialPrice
-        ) {
-          await bot.telegram.sendMessage(
-            subscription.chatId,
-            "Hey! We've Noticed a price drop on your product."
-          );
-          await bot.telegram.sendPhoto(
-            subscription.chatId,
-            subscription.imageUrl,
-            {
-              parse_mode: "Markdown",
-              reply_markup: {
-                inline_keyboard: [
-                  [
-                    {
-                      text: "Delete Alert?",
-                      callback_data: "delete_alert_" + subscription._id,
-                      hide: false,
-                    },
+        if (String(pricing).match(/\d+/)) {
+          if (
+            Number(pricing) < subscription.currentPrice &&
+            subscription.currentPrice <= subscription.initialPrice
+          ) {
+            await bot.telegram.sendMessage(
+              subscription.chatId,
+              "Hey! We've Noticed a price drop on your product."
+            );
+            await bot.telegram.sendPhoto(
+              subscription.chatId,
+              subscription.imageUrl,
+              {
+                parse_mode: "Markdown",
+                reply_markup: {
+                  inline_keyboard: [
+                    [
+                      {
+                        text: "Delete Alert?",
+                        callback_data: "delete_alert_" + subscription._id,
+                        hide: false,
+                      },
+                    ],
+                    [
+                      {
+                        text: "View Price History?",
+                        callback_data: "pricehistory_" + subscription._id,
+                        hide: false,
+                      },
+                    ],
                   ],
-                ],
-              },
-              caption: `*Title: *${subscription.title}\n\n*Previous Price: *₹${subscription.currentPrice}\n\n*Current Price: *₹${pricing}\n\n*Url: *${subscription.url}`,
-            }
-          );
-        }
-        if (subscription.currentPrice != pricing)
-          subscription.priceHistories.push({
-            datetime: new Date(),
-            price: pricing,
-          });
+                },
+                caption: `*Title: *${subscription.title}\n\n*Previous Price: *₹${subscription.currentPrice}\n\n*Current Price: *₹${pricing}\n\n*Url: *${subscription.url}`,
+              }
+            );
+          }
+          
+          if (subscription.currentPrice != pricing)
+            subscription.priceHistories.push({
+              datetime: new Date(),
+              price: pricing,
+            });
 
-        subscription.currentPrice = pricing;
-        await subscription.save();
+          subscription.currentPrice = pricing;
+          await subscription.save();
+        }
       }
     } catch (e) {
       console.log("Cron error:", e);
