e49a3db9d01e181a23443f3e0d9eb1bfa2f59a09
Mukul Shakya <muklshakya123@gmail.com>
2021-05-01 13:00:46 +0530

    Added ping route

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
 const { Telegraf } = require("telegraf");
-require("dotenv");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 // bot.use(Telegraf.log());
 const db = require("./src/db");
+
+// EXPRESS SERVER
+const app = express();
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) =>
+  res.status(200).json({
+    message: `Server up on ${port}`,
+  })
+);
+app.listen(port, () => console.log(`Server up on ${port}`));
 
 // BOT SERVER LISTENER
 bot
