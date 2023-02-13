4e9524d2bbdaca445f7a26d4516eda15947ca6e8
Mukul Shakya <muklshakya123@gmail.com>
2023-02-13 07:23:48 +0530

    Added config for cyclic

--- index.js
+++ index.js
@@ -7,13 +7,24 @@ const Agenda = require("agenda");
 const db = require("./src/db");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 // bot.use(Telegraf.log());
+const env = process.env.NODE_ENV || "local";
+let botOptions = {};
+if (env === "production") {
+  botOptions = {
+    webhook: {
+      domain: process.env.CYCLIC_URL,
+      port: process.env.PORT || 3000,
+      secretToken: process.env.SECRET,
+    },
+  };
+}
 
 // INITIALIZE JOB QUEUE AFTER MONGOOSE CONNECTION
 mongoose.connection.on("connected", async () => {
   console.log("mongo connection open");
   try {
     // BOT SERVER LISTENER
-    await bot.launch();
+    await bot.launch(botOptions);
     console.log("BOT Started Successfully!");
     // INITIALIZE BOT ACTIONS
     require("./src/telegram")(bot, db);
